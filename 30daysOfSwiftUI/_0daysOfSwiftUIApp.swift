56cf43da4c6ccef606903abf9bb1b459c8d0cc5f
= <malisanjay5433@gmail.com>
2021-07-06 14:57:27 +0530

    Initial Commit

---
+++ 30daysOfSwiftUI/_0daysOfSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0daysOfSwiftUIApp.swift
+//  30daysOfSwiftUI
+//
+//  Created by Sanjay Mali on 06/07/21.
+//
+
+import SwiftUI
+
+@main
+struct _0daysOfSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
